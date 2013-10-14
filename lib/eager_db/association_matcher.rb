1d5bfa2d24e9daa33fb900a40d442397c0b45265
wangjohn <wangjohn@mit.edu>
2013-10-14 16:10:21 -0500

    Working on the association matcher

---
+++ lib/eager_db/association_matcher.rb
@@ -0,0 +1,68 @@
+module EagerDB
+  class SqlStatement
+    attr_reader :raw_sql, :bind_values
+
+    def initialize(raw_sql, bind_values)
+      @raw_sql = raw_sql
+      @bind_values = bind_values
+    end
+  end
+
+  class AssociationMatcher
+    class << self
+
+      attr_reader :match_sql, :preloads
+
+      def match_on(sql, bind_values)
+        @match_sql = SqlStatement.new(sql, bind_values)
+      end
+
+      def preload(sql, bind_values)
+        (@preloads ||= []) << SqlStatement.new(sql, bind_values)
+      end
+
+      def result
+        @result ||= AssociationMatcherResult.new(self)
+      end
+
+      def result_variable?(name)
+        true
+      end
+    end
+  end
+
+  class AssociationMatcherResultVariable
+    def initializer(result, name)
+      @result = result
+      @name = name
+    end
+  end
+
+  class AssociationMatcherResult
+    attr_reader :matcher
+
+    def initialize(matcher)
+      @matcher = matcher
+    end
+
+    def get_variable(name)
+      AssociationMatcherResultVariable.new(matcher, name)
+    end
+
+    def method_missing(method, *args, &block)
+      if matcher.result_variable?(method)
+        get_variable(name)
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      if matcher.result_variable?(method)
+        true
+      else
+        super
+      end
+    end
+  end
+end
