995049fb1f429d81c5e0155cf4cf920b0b23bfd4
wangjohn <wangjohn@mit.edu>
2013-11-24 20:36:29 -0600

    Changing up abstract processor.

--- lib/eager_db/processors/abstract_processor.rb
+++ lib/eager_db/processors/abstract_processor.rb
@@ -1,20 +1,26 @@
 module EagerDB
   module Processors
     class AbstractProcessor
-      def initialize
-        @match_statements = []
+      def initialize(match_statement)
+        @match_statement = match_statement
+        @preload_statements = []
       end
 
-      def add_match_statement(match_statement)
-        @match_statements << match_statement
+      def add_preload_statement(preload_statement)
+        @preload_statements << preload_statement
       end
 
       def process_preloads(sql, result)
-        preloads = @match_statements.collect do |statement|
-          statement.process(sql, result)
-        end
+        if @match_statement.matches?(sql)
+          options = {
+            result: result,
+            sql_statement: @match_statement
+          }
 
-        preloads.flatten
+          @preload_statements.collect do |statement|
+            statement.inject_values(options)
+          end
+        end
       end
     end
   end
