75da233d3e8e9b899834a62cd52ba7b6854b7b94
wangjohn <wangjohn@mit.edu>
2013-11-29 19:38:56 -0500

    Finishing up database endpoint tests.

--- test/endpoints_test.rb
+++ test/endpoints_test.rb
@@ -1,7 +1,6 @@
 require './helper'
 
-class EndpointsTest < EagerDB::Test
-
+class AbstractEndpointTest < EagerDB::Test
   def test_abstract_endpoint_cannot_be_called
     message = EagerDB::Message.new('payload')
 
@@ -9,5 +8,36 @@ class EndpointsTest < EagerDB::Test
       endpoint = EagerDB::Endpoints::AbstractEndpoint.new
       endpoint.process_payload(message)
     end
+  end
+end
+
+class DatabaseEndpointTest < EagerDB::Test
+  def setup
+    @history = []
+    @db_proc = Proc.new { |sql| @history << sql }
+    @endpoint = EagerDB::Endpoints::DatabaseEndpoint.new(@db_proc)
+  end
+
+  def test_simple_proc
+    query = "SELECT * FROM users WHERE id = 5"
+    message = EagerDB::Message.new(query)
+    @endpoint.process_payload(message)
+
+    assert_equal [query], @history
+  end
+
+  def test_multiple_queries
+    q1 = "First query"
+    q2 = "Second query"
+    q3 = "This is the third query"
+
+    @endpoint.process_payload(EagerDB::Message.new(q1))
+    assert_equal [q1], @history
+
+    @endpoint.process_payload(EagerDB::Message.new(q2))
+    assert_equal [q1, q2], @history
+
+    @endpoint.process_payload(EagerDB::Message.new(q3))
+    assert_equal [q1, q2, q3], @history
   end
 end
