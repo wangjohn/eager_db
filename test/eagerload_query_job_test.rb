2a034bd3fbe81aeaf4c0706c7df69b90562328b4
wangjohn <wangjohn@mit.edu>
2013-12-01 00:48:06 -0500

    Finishing up eagerloadQueryJob tests.

--- test/eagerload_query_job_test.rb
+++ test/eagerload_query_job_test.rb
@@ -4,15 +4,7 @@ class EagerloadQueryJobTest < EagerDB::Test
   def setup
     @aggregator = create_aggregator
     @channel = create_communication_channel(@aggregator)
-
-    options = {
-      sql: "SELECT * FROM users WHERE name = 'ryan'",
-      result: {id: 12345, name: 'ryan', brother: 'john'}, 
-      processor_aggregator: @aggregator,
-      communication_channel: @channel
-    }
-    @job = EagerDB::EagerloadQueryJob.new(options)
-  end
+ end
 
   def create_aggregator
     aggregator = EagerDB::ProcessorAggregator::AbstractProcessorAggregator.new
@@ -43,6 +35,51 @@ class EagerloadQueryJobTest < EagerDB::Test
     end
   end
 
-  def test_job_with_no_processors_in_aggregator
+  def test_job_which_does_not_match_in_aggregator
+    job = EagerDB::EagerloadQueryJob.new({
+      sql: "SELECT * FROM blahblahs WHERE name = 'david' GROUP BY company",
+      result: {id: 12345, name: 'ryan', brother: 'john'}, 
+      processor_aggregator: @aggregator,
+      communication_channel: @channel
+    })
+    job.work
+
+    assert_equal 0, @database_history.length
+  end
+
+  def test_job_which_matches_a_single_processor_in_aggregator
+    job = EagerDB::EagerloadQueryJob.new({
+      sql: "SELECT * FROM users WHERE name = 'ryan'",
+      result: {id: 12345, name: 'ryan', brother: 'john'}, 
+      processor_aggregator: @aggregator,
+      communication_channel: @channel
+    })
+    job.work
+
+    assert_equal 1, @database_history.flatten.length
+    assert_equal ["SELECT * FROM products WHERE user_name = 'ryan'"], @database_history.flatten
+  end
+
+  def test_multiple_job_runs_on_an_aggregator
+    job1 = EagerDB::EagerloadQueryJob.new({
+      sql: "SELECT * FROM users WHERE name = 'ryan'",
+      result: {id: 12345, name: 'ryan', brother: 'john'}, 
+      processor_aggregator: @aggregator,
+      communication_channel: @channel
+    })
+    job1.work
+
+    job2 = EagerDB::EagerloadQueryJob.new({
+      sql: "SELECT * FROM users WHERE name = 'CRAZYMAN'",
+      result: {id: 54321, name: 'CRAZYMAN', brother: 'CRAZYDUDE'},
+      processor_aggregator: @aggregator,
+      communication_channel: @channel
+    })
+    job2.work
+
+    result = @database_history.flatten
+    assert_equal 2, result.length
+    assert_equal "SELECT * FROM products WHERE user_name = 'ryan'", result[0]
+    assert_equal "SELECT * FROM products WHERE user_name = 'CRAZYMAN'", result[1]
   end
 end
