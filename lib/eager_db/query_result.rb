a5dcc22895301f64ffeab26d967bd6c6a8934391
wangjohn <wangjohn@mit.edu>
2013-12-04 22:25:08 -0500

    Adding a QueryResult error.
    TODO: this error needs context.

--- lib/eager_db/query_result.rb
+++ lib/eager_db/query_result.rb
@@ -1,4 +1,7 @@
 module EagerDB
+  class QueryResultError < StandardError
+  end
+
   class QueryResult
     def initialize(values_mapping = {})
       values_mapping.each do |name, value|
@@ -10,6 +13,12 @@ module EagerDB
       self.class.instance_eval do
         define_method(name) { value }
       end
+    end
+
+    def method_missing(method, *args, &block)
+      # TODO: This is a bad error message. We should give the user some context
+      # about which QueryResult he is looking at.
+      raise QueryResultError, "The attribute `#{method}` does not exist for this QueryResult. Try checking to make sure your match statements and preload statements conform to your database schema. Especially check to make that instances of `match_result.some_attribute_name` occur with a valid attribute name."
     end
   end
 end
