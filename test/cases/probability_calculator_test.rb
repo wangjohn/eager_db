a8b9fc67ef9fb4d222f2617ae6df3179bd57045f
wangjohn <wangjohn@mit.edu>
2013-12-09 00:02:14 -0500

    Wrote a test with actual sql for probability calculator.
    Closes #11

--- test/cases/probability_calculator_test.rb
+++ test/cases/probability_calculator_test.rb
@@ -96,4 +96,42 @@ class ProbabilityCalculatorTest < EagerDB::Test
     assert_equal 1, transitions.length
     assert_equal ["another sql statement"], transitions["some sql statement"]
   end
+
+  def test_single_user_log_files_for_sql_with_binds
+    t = Time.now
+    log_files = [
+      EagerDB::Prediction::SqlLog.new("SELECT * FROM users WHERE name = 'ryan'", t, "user1"),
+      EagerDB::Prediction::SqlLog.new("SELECT * FROM chairs WHERE brand = 'ikea' AND type = 'mahogany'", t + 1, "user1"),
+      EagerDB::Prediction::SqlLog.new("SELECT * FROM tables WHERE owner = 'ryan'", t + 4, "user1"),
+      EagerDB::Prediction::SqlLog.new("SELECT * FROM users WHERE name = 'john'", t + 15, "user1"),
+      EagerDB::Prediction::SqlLog.new("SELECT * FROM tables WHERE owner = 'john'", t + 18, "user1"),
+    ]
+
+    calculator = EagerDB::Prediction::ProbabilityCalculator.new(log_files, 10)
+    transitions = calculator.likely_transitions
+
+    users_probs = calculator.probability_storage[
+      "SELECT * FROM users WHERE name = ?"].probabilities
+    assert_equal 2, users_probs.length
+    assert_equal 1.0, users_probs[
+      "SELECT * FROM tables WHERE owner = ?"]
+    assert_equal 0.5, users_probs[
+      "SELECT * FROM chairs WHERE brand = ? AND type = ?"]
+
+    chairs_probs = calculator.probability_storage[
+      "SELECT * FROM chairs WHERE brand = ? AND type = ?"].probabilities
+    assert_equal 1, chairs_probs.length
+    assert_equal 1.0, chairs_probs[
+      "SELECT * FROM tables WHERE owner = ?"]
+
+    tables_probs = calculator.probability_storage[
+      "SELECT * FROM tables WHERE owner = ?"].probabilities
+    assert_equal 0, tables_probs.length
+
+    assert_equal 2, transitions.length
+    assert_equal ["SELECT * FROM tables WHERE owner = ?"], transitions[
+      "SELECT * FROM users WHERE name = ?"]
+    assert_equal ["SELECT * FROM tables WHERE owner = ?"], transitions[
+      "SELECT * FROM chairs WHERE brand = ? AND type = ?"]
+  end
 end
